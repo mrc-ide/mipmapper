51399fb70f95958dffa5b8d8483b1c03a01ab661
bobverity <r.verity@imperial.ac.uk>
2018-05-18 15:34:57 +0100

    added roxygen

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,25 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+
+#------------------------------------------------
+# The following commands ensure that package dependencies are listed in the
+# NAMESPACE file.
+
+#' @useDynLib mipmapper
+NULL
+
+#------------------------------------------------
+#' @title Dummy function
+#'
+#' @description Dummy function
+#'
+#' @details TODO
+#'
+#' @param x TODO
+#'
+#' @export
+#' @examples
+#' # TODO
 
 hello <- function() {
   print("Hello, world!")
 }
+
