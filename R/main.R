7507112ac96b48f8a585e57bfa0e0e27c622da3a
OJWatson <o.watson15@imperial.ac.uk>
2018-05-23 20:47:54 +0100

    impute mip data with tests

--- R/main.R
+++ R/main.R
@@ -8,7 +8,8 @@
 #' @import graphics
 NULL
 
-#------------------------------------------------
+
+#  filter_misc -----------------------------------------------------------------
 #' @title Filter data
 #'
 #' @description Filter out miscellaneous unwanted values from data
@@ -24,7 +25,10 @@ NULL
 #' @examples
 #' # TODO
 
-filter_misc <- function(dat, SNP_only = TRUE, group_Alt = TRUE, drop_irregular = TRUE) {
+filter_misc <- function(dat, 
+                        SNP_only = TRUE, 
+                        group_Alt = TRUE, 
+                        drop_irregular = TRUE) {
   
   # TODO - check format of dat
   
@@ -37,7 +41,8 @@ filter_misc <- function(dat, SNP_only = TRUE, group_Alt = TRUE, drop_irregular =
   if (group_Alt) {
     dat2 <- dat %>%
       group_by(.dots = setdiff(names(dat), c("Alt", "Barcode_Count"))) %>%
-      summarize(Alt = paste(Alt, collapse = ","), Barcode_Count = sum(Barcode_Count))
+      summarize(Alt = paste(Alt, collapse = ","), 
+                Barcode_Count = sum(Barcode_Count))
     dat2 <- as.data.frame(dat2)
     dat <- dat2[,names(dat)]
   }
@@ -53,7 +58,8 @@ filter_misc <- function(dat, SNP_only = TRUE, group_Alt = TRUE, drop_irregular =
   invisible(dat)
 }
 
-#------------------------------------------------
+
+#  filter_coverage -------------------------------------------------------------
 #' @title Filter data
 #'
 #' @description Filter data based on coverage
@@ -79,7 +85,8 @@ filter_coverage <- function(dat, min_coverage = 2) {
   invisible(dat)
 }
 
-#------------------------------------------------
+
+#  melt_mip_data ---------------------------------------------------------------
 #' @title Melt mip data
 #'
 #' @description Melt the mip data into a data frame with one column per locus. 
@@ -120,21 +127,24 @@ melt_mip_data <- function(dat) {
                      "Coverage", "Barcode_Count", "ID")
   meta <- which(!names(dat) %in% depth_related)
   
+  # the total width of our final dataset
+  cols <- length(unique(dat$ID)) + length(meta)
   
-  df <- data.frame(matrix(ncol = length(unique(dat$ID)) + length(meta), nrow = 0))
+  df <- data.frame(matrix(ncol = cols, nrow = 0))
   colnames(df) <- c(names(dat)[meta],unique(dat$ID))
   
   s <- unique(dat$Sample_ID)
-  slist <- list(); length(slist) <- length(s)+1
+  slist <- list()
+  length(slist) <- length(s) + 1
   slist[[1]] <- df
   
   for(i in 1:length(s)){
-    d <- dat[dat$Sample_ID==s[i],]  
+    d <- dat[dat$Sample_ID==s[i], ]  
     df <- data.frame(matrix(ncol = length(d$Barcode_Count),
                             nrow=1, 
                             data = d$Barcode_Count / d$Coverage))
     names(df) <- d$ID
-    df <- cbind(d[1, ..meta, drop = FALSE], df)
+    df <- cbind(d[1, meta, drop = FALSE], df)
     slist[[i+1]] <- df
   }
   
@@ -143,3 +153,53 @@ melt_mip_data <- function(dat) {
   
 }
 
+
+#  impute_mip_data -------------------------------------------------------------
+#' @title Impute missing mip data
+#'
+#' @description Using the output of \code{melt_mip_data}, missing values are 
+#'  imputed by applying a summary function to the non NA values for a locus. 
+#'  The default summary function takes the mean of the non NA samples.
+#'  
+#'
+#' @param dat output of \code{melt_mip_data}
+#' @param FUN function to impute missing values. Default = `mean`
+#' @param ... other arguments to pass to FUN. 
+#'
+#' @return Invisibly returns the mip data frame with missing values imputed
+#' @export
+#' @examples
+#' 
+#' dat <- data.frame(
+#' "Sample_ID" = c(rep("a", 3), rep("b", 2)),
+#' "Chrom" = c(1, 1, 2, 1, 1),
+#' "Pos" = c(100, 200, 50, 100, 200),
+#' "Coverage" = c(47, 95, 100, 52, 100),
+#' "Barcode_Count" = c(47, 0, 40, 52, 70)
+#' )
+#' 
+#' dat <- melt_mip_data(dat = dat)
+#' dat <- impute_mip_data(dat = dat)
+#' 
+
+impute_mip_data <- function(dat, FUN = mean, ...) {
+  
+  # get which columns refer to loci and which refer to variable names
+  datNames <- names(dat)[which(!grepl("^chr.*", names(dat)))]
+  locusNames <- setdiff(names(dat), datNames)
+  
+  ## TODO: Discuss at some point standard variable handling, where PCs will be
+  # subset out positive controls
+  # dat <- subset(dat, Study!="PC")
+  
+  # impute NAs
+  dat_mat <- as.matrix(dat[, locusNames])
+  locus_impute <- apply(dat_mat, 2, FUN, na.rm = TRUE, ...)
+  locus_impute <- outer(rep(1, nrow(dat_mat)), locus_impute)
+  dat_mat[is.na(dat_mat)] <- locus_impute[is.na(dat_mat)]
+  dat <- cbind(dat[, datNames], dat_mat)
+  
+  # return invisbly
+  invisible(dat)
+  
+}
--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -27,3 +27,28 @@ test_that("`melt_mip_data()` works", {
   )
   
 })
+
+
+test_that("`impute_mip_data()` works", {
+  
+  dat <- data.frame(
+    Sample_ID = c(rep("a", 3), rep("b", 2)), 
+    Chrom = c("chr1","chr1", "chr2", "chr1", "chr1"), 
+    Pos = c(100, 200, 50, 100, 200), 
+    Coverage = c(47, 95, 100, 52, 100), 
+    Barcode_Count = c(47, 0, 40, 52, 70)
+  )
+  
+  dat <- melt_mip_data(dat = dat)
+  dat <- impute_mip_data(dat = dat)
+  expect_equal(
+    dat,
+    structure(c(1, 2, 1, 1, 0, 0.7, 0.4, 0.4), 
+              .Dim = c(2L, 4L), 
+              .Dimnames = list(NULL, 
+                               c("", "chr1_100", "chr1_200", "chr2_50")
+              )
+    )
+  )
+  
+})
