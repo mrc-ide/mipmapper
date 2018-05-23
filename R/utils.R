e245932dcd4ffd7c0423bf6b36cd63ae356ff1c2
OJWatson <o.watson15@imperial.ac.uk>
2018-05-23 19:39:54 +0100

    added pipe for ease for writing internal functions

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+#' Pipe operator
+#'
+#' See \code{\link[magrittr:pipe]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @export
+#' @usage lhs \%>\% rhs
+NULL
